142b298d890dd7646e8ba957efd4e02bbea3b2a8
sakeev92 <vs.sakeev@mail.ru>
2023-01-16 18:40:42 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,116 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        System.out.println("Первый массив");
+        int[] numbers1 = new int[3];
+        for (int i = 0; i < numbers1.length; i++) {
+            numbers1[i] = i + 1;
+            System.out.println(numbers1[i]);
+        }
+        System.out.println("Второй массив");
+        double[] numbers2 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < numbers2.length; i++) {
+            System.out.println(numbers2[i]);
+        }
+        System.out.println("Третий массив");
+        int[] numbers3 = new int[]{48963, 456, 235, 4, 5, 8, 89};
+        for (int i = 0; i < numbers3.length; i++) {
+            System.out.println(numbers3[i]);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        System.out.println("Первый массив");
+        int[] numbers1 = new int[3];
+        {
+            for (int i = 0; i < numbers1.length; i++) {
+                numbers1[i] = i + 1;
+                System.out.print(numbers1[i]);
+                if (i < numbers1.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+
+            System.out.println();
+            System.out.println("Второй массив");
+            double[] numbers2 = {1.57, 7.654, 9.986};
+            for (int i = 0; i < numbers2.length; i++) {
+                System.out.print(numbers2[i]);
+                if (i < numbers2.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+            System.out.println();
+            System.out.println("Третий массив");
+            int[] numbers3 = new int[]{48963, 456, 235, 4, 5, 8, 89};
+            for (int i = 0; i < numbers3.length; i++) {
+                System.out.print(numbers3[i]);
+                if (i < numbers3.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        System.out.println("Первый массив");
+        int[] numbers1 = new int[3];
+        {
+            for (int i = numbers1.length - 1; i >= 0; i--) {
+                numbers1[i] = i + 1;
+                System.out.print(numbers1[i] + " ");
+                if (i < numbers1.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+
+            System.out.println();
+            System.out.println("Второй массив");
+            double[] numbers2 = {1.57, 7.654, 9.986};
+            for (int i = numbers2.length - 1; i >= 0; i--) {
+                System.out.print(numbers2[i]);
+                if (i < numbers2.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+            System.out.println();
+            System.out.println("Третий массив");
+            int[] numbers3 = new int[]{48963, 456, 235, 4, 5, 8, 89};
+            for (int i = 0; i < numbers3.length; i++) {
+                System.out.print(numbers3[i]);
+                if (i < numbers3.length - 1) {
+                    System.out.print(", ");
+
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int [] arr = {2, 4, 6, 7 , 9};
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
